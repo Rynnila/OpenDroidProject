7cc7d126a9796285e6a5ce34c3ecc0ecf2511830
Rynnila <alinnyrc6@gmail.com>
2024-08-14 12:08:53 -0300

    Criando a estrutura base do projeto
    Enviando os arquivos do SpringInitialzr

---
+++ src/main/java/br/gov/sp/fatec/opendroid/OpenDroid/OpenDroidApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.opendroid.OpenDroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDroidApplication.class, args);
+	}
+
+}
